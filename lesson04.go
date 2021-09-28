c0b38e91497a6bfe9f6ec014d88b2de8327b72f7
nbr41to <nbr.41to@gmail.com>
2021-09-29 00:40:05 +0900

    .

--- lesson04.go
+++ lesson04.go
@@ -1,31 +1,37 @@
+/*
+	04. 条件分岐 if文
+	scriptの実行: $ go run lesson04.go
+*/
 package main
 
 import (
 	"fmt"
 )
 
-/* if文 */
-
 func main() {
 	/* 条件演算子 */
-	fmt.Println("GO" == "go") // false
-	fmt.Println("GO" != "go") // true
+	const lang = "go"
+	fmt.Println(lang == "go") // true
+	fmt.Println(lang != "go") // false
 
 	score := 60
 	fmt.Println(score > 60)  // true
 	fmt.Println(score >= 60) // true
 	fmt.Println(score < 60)  // false
 	fmt.Println(score <= 60) // false
-	fmt.Println(score == 60) // false
-	fmt.Println(score != 60) // true
 
 	/* 論理演算子 */
-	fmt.Println(score >= 60 && score <= 30) // false
-	fmt.Println(score >= 60 || score <= 30) // true
+	// かつ（AND）
+	fmt.Println(score <= 30 && 80 <= score) // false
+	// または（OR）
+	fmt.Println(lang == "go" || score > 80) // true
+	// ではない（NOT）
+	fmt.Println(!false && !(lang == "go")) // false
 
+	/* if文 */
 	x, y := 1, 2
 
-	if x < y {
+	if x > y {
 		fmt.Println("xはyより大きい")
 	} else if x == y {
 		fmt.Println("xとyは等しい")
@@ -33,12 +39,26 @@ func main() {
 		fmt.Println("xはyより大きくない🥺")
 	}
 
-	/* goto */
+	/* goto文 */
 	goto Here // 指定したタグがあれば、そこに飛ぶ
 	fmt.Println("切ない感情")
 Here:
 	fmt.Println("嬉しい感情")
+
+	/* ifとgotoを組み合わせてみる */
+	status := "step2"
+	if (status == "not started") || (status == "step1") {
+		goto Initialized
+	} else if status == "step2" {
+		goto SecondPhase
+	}
+Initialized:
+	fmt.Println("0.卵を割る")
+	fmt.Println("1.卵を焼く")
+SecondPhase:
+	fmt.Println("2.ケチャップをかける")
+	fmt.Println("3.完成")
 }
 
-// 省略形などの書き方はない
-// 誰が書いても同じになるのがGoの特徴
+// MEMO
+// 省略形などの書き方はなく, 誰が書いても同じになるのがGoの特徴
--- lesson07.go
+++ lesson07.go
@@ -1,18 +1,37 @@
+/*
+	07. map（JavaScriptで言うところのオブジェクト）
+	scriptの実行: $ go run lesson07.go
+*/
 package main
 
 import (
 	"fmt"
 )
 
-/* map（object） */
-
 func main() {
-	user := map[string]string{
-		"name": "pienman",
-		"age":  "7", // string
+	/* 定義 */
+	user := map[string]string{ // `map[string]string` ...keyがstringで値がstringのmap
+		"name": "ピエン 翼",
+		"age":  "7",
 	}
-	fmt.Println(user["name"])
+	fmt.Println(user["name"]) // 呼び出し
 
+	/* 要素の追加 */
+	user["tell"] = "090-1234-5678"
+	fmt.Println(user)
+
+	/* 要素の削除 */
+	delete(user, "age")
+	fmt.Println(user)
+
+	/* keyの値と有無を調べる */
+	if value, ok := user["age"]; ok {
+		fmt.Printf("age is exist.value is %v\n", value)
+	} else {
+		fmt.Println("age is not exist.")
+	}
+
+	/* mapの型を定義 */
 	type userType struct {
 		name  string
 		age   int
@@ -20,11 +39,10 @@ func main() {
 	}
 
 	user1 := userType{
-		name:  "pienman",
+		name:  "ピエン 翼",
 		age:   7,
 		login: true,
 	}
 
 	fmt.Println(user1.name, "is login.")
-
 }
